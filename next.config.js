307ff49aae5037a8e12be1a375e198e9ffb15cf2
Gunj Joshi <gunjjoshi@Gunj-Joshis-MacBook.local>
2024-02-03 01:24:16 +0530

    new cors way

--- next.config.js
+++ next.config.js
@@ -2,8 +2,34 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ['lh3.googleusercontent.com'],
+      domains: ['lh3.googleusercontent.com'],
     },
-}
-
-module.exports = nextConfig
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Access-Control-Allow-Origin',
+              value: '*', 
+            },
+            {
+              key: 'Access-Control-Allow-Headers',
+              value: 'Origin, X-Requested-With, Content-Type, Accept',
+            },
+            {
+              key: 'Access-Control-Allow-Methods',
+              value: 'GET, POST, PUT, DELETE, OPTIONS',
+            },
+            {
+              key: 'Access-Control-Allow-Credentials',
+              value: 'true',
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
+  
